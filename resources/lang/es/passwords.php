a40c7f74f193c557438791053dd7451d5364ffec
Alv-ar <77983145+Alv-ar@users.noreply.github.com>
2021-12-17 17:09:13 +0100

    feat: added some traductions

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu constraseña ha sido actualizada!',
+    'sent' => 'Te hemos enviado un email con el link de reseteo de contraseña!',
+    'throttled' => 'Porfavor espera antes de volverlo a intentar.',
+    'token' => 'Este token de reseteo de contraseña es invalido.',
+    'user' => "No existen usuarios con ese correo electronico.",
+
+];
